3a5d9da1f994ed00852b0314b811f3b08cd3b5a5
NCHAUPE SOLOMON SETSHEDI <nsetshedi.ss@gmail.com>
2020-10-03 11:56:31 +0200

    3rd Commit "Javascript Complete"

--- draw.js
+++ draw.js
@@ -11,3 +11,23 @@ sizePicker.onsubmit = function(event){
     clearGrid();
     makeGrid();
 };
+
+function makeGrid() {
+    for (let r=0; r<height.value; r++){
+        const row = canvas.insertRow(r);
+        for (let c=0; c<width.value; c++){
+            const cell = row.insertCell(c);
+            cell.addEventListener("click", fillSquare);
+        }
+    }
+}
+
+function clearGrid() {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+}
+
+function fillSquare() {
+    this.setAttribute("style", `background-color: ${color.value}`);
+}
